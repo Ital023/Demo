28f567e0fd10edf0d16d22a18fa55ade15fe31e3
Ital023 <contadoitalo911@gmail.com>
2026-01-30 14:04:02 -0300

    initializing the repository

---
+++ apps/backend/src/main/java/io/github/Ital023/ProductListingApi/ProductListingApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.ProductListingApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductListingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductListingApiApplication.class, args);
+	}
+
+}
---
+++ apps/backend/src/test/java/io/github/Ital023/ProductListingApi/ProductListingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.ProductListingApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductListingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
